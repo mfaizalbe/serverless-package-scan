7660c5e0bb34f8c69af30d3360f512b27297ec2f
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-21 21:34:28 +0800

    initial commit

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
-    key    = "package-vul-scan-luqman.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
+    key    = "package-vul-scan-faizal.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
     region = "us-east-1"
   }
 }
